dcad2372a907d4a48185d8fdc4537073e417d24d
Bartek Foszmanowicz <madmatrixfcb@gmail.com>
2016-12-17 17:43:12 +0100

    SekwencjeTest - Zmiana przy wyjatkach, kolejnosc sprawdzania

--- src/test/java/lab2_1/SekwencjeTest.java
+++ src/test/java/lab2_1/SekwencjeTest.java
@@ -8,31 +8,35 @@ import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
 
 public class SekwencjeTest {
-
+	
 	@Test(expected = IllegalArgumentException.class)
 	public void JestWSekwencjiTest() {
 		int key = 3;
 		int[] seq = { 0, 2 };
-		SearchResult searchResult;
-		searchResult = BinarySearch.search(key, seq);
+		
 		if (seq.length == 0) {
 			throw new IllegalArgumentException("Dlugosc sekwencji wynosi 0");
 		}
+		SearchResult searchResult;
+		searchResult = BinarySearch.search(key, seq);
 		assertTrue(searchResult.isFound());
 		assertEquals(0, searchResult.getPosition());
 	}
-
+	
 	@Test(expected = IllegalArgumentException.class)
 	public void NieJestWSekwencjiTest() {
 		int key = 3;
 		int[] seq = { 2 };
-		SearchResult searchResult;
-		searchResult = BinarySearch.search(key, seq);
+		
 		if (seq.length == 0) {
 			throw new IllegalArgumentException("Dlugosc sekwencji wynosi 0");
 		}
+		SearchResult searchResult;
+		searchResult = BinarySearch.search(key, seq);
 		assertFalse(searchResult.isFound());
 		assertEquals(-1, searchResult.getPosition());
 	}
+	
+
 
 }
