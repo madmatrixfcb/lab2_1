70a7d5fae058af00e8c17b6a738da187da1898f7
Bartek Foszmanowicz <madmatrixfcb@gmail.com>
2016-12-17 17:12:39 +0100

    Dodanie drugiego testu - NieJestWSekwencjiTest

--- src/test/java/lab2_1/BinarySearchTest.java
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -1,13 +1,14 @@
 package lab2_1;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
 
 public class BinarySearchTest {
-
+	
 	@Test(expected = IllegalArgumentException.class)
 	public void JestWSekwencjiTest() {
 		int key = 3;
@@ -19,6 +20,19 @@ public class BinarySearchTest {
 		}
 		assertTrue(searchResult.isFound());
 		assertEquals(0, searchResult.getPosition());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void NieJestWSekwencjiTest() {
+		int key = 3;
+		int[] seq = { 2 };
+		SearchResult searchResult;
+		searchResult = BinarySearch.search(key, seq);
+		if (seq.length == 0) {
+			throw new IllegalArgumentException("Dlugosc sekwencji wynosi 0");
+		}
+		assertFalse(searchResult.isFound());
+		assertEquals(-1, searchResult.getPosition());
 	}
 
 }
