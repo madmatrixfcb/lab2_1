d6b161756adfbda31467b76484bff5cddf44b3f2
Bartek Foszmanowicz <madmatrixfcb@gmail.com>
2017-01-14 19:08:15 +0100

    Dodanie testu ElementZeroTest

--- src/test/java/lab2_1/ElementyTest.java
+++ src/test/java/lab2_1/ElementyTest.java
@@ -30,7 +30,7 @@ public class ElementyTest {
 		assertEquals(1, wynikWyszukiwania.getPosition());
 		assertTrue(wynikWyszukiwania.isFound());
 	}
-	
+
 	@Test
 	public void OstatniElementTest() {
 		int key = 7;
@@ -40,6 +40,7 @@ public class ElementyTest {
 		assertEquals(2, wynikWyszukiwania.getPosition());
 		assertTrue(wynikWyszukiwania.isFound());
 	}
+
 	@Test
 	public void ElementPozaTest() {
 		int key = 9;
@@ -49,7 +50,7 @@ public class ElementyTest {
 		assertEquals(-1, wynikWyszukiwania.getPosition());
 		assertFalse(wynikWyszukiwania.isFound());
 	}
-	
+
 	@Test
 	public void PojElemJestWSekwencjiTest() {
 		int key = 2;
@@ -69,39 +70,38 @@ public class ElementyTest {
 		assertFalse(wynikWyszukiwania.isFound());
 		assertEquals(-1, wynikWyszukiwania.getPosition());
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void PustaSekwencjaTest() {
-		int [] seq = { };
-		if (seq.length == 0)
-		{
+		int[] seq = {};
+		if (seq.length == 0) {
 			throw new IllegalArgumentException("Dlugosc sekwencji wynosi 0");
 		}
-		
+
 	}
-	
+
 	@Test
 	public void SekwencjaMaxPlusJedenTest() {
 		int key = 2;
-		int max = Integer.MAX_VALUE+1;
+		int max = Integer.MAX_VALUE + 1;
 		int[] seq = { 2, 5, 7, 9, max };
 		SearchResult wynikWyszukiwania;
 		wynikWyszukiwania = BinarySearch.search(key, seq);
 		assertTrue(wynikWyszukiwania.isFound());
 		assertEquals(0, wynikWyszukiwania.getPosition());
 	}
-	
+
 	@Test
 	public void SekwencjaMinMinusJedenTest() {
 		int key = 2;
-		int min = Integer.MIN_VALUE-1;
+		int min = Integer.MIN_VALUE - 1;
 		int[] seq = { min, -5, -2, 0, 2, 5, 7 };
 		SearchResult wynikWyszukiwania;
 		wynikWyszukiwania = BinarySearch.search(key, seq);
 		assertTrue(wynikWyszukiwania.isFound());
 		assertEquals(4, wynikWyszukiwania.getPosition());
 	}
-	
+
 	@Test
 	public void ElementUjemnyTest() {
 		int key = -2;
@@ -110,6 +110,16 @@ public class ElementyTest {
 		wynikWyszukiwania = BinarySearch.search(key, seq);
 		assertTrue(wynikWyszukiwania.isFound());
 		assertEquals(2, wynikWyszukiwania.getPosition());
+	}
+
+	@Test
+	public void ElementZeroTest() {
+		int key = 0;
+		int[] seq = { -7, -5, -2, 0, 2, 5, 7 };
+		SearchResult wynikWyszukiwania;
+		wynikWyszukiwania = BinarySearch.search(key, seq);
+		assertTrue(wynikWyszukiwania.isFound());
+		assertEquals(3, wynikWyszukiwania.getPosition());
 	}
 
 }
