fd947267856fc849491ceda746d5f1d042b70c1c
Bartek Foszmanowicz <madmatrixfcb@gmail.com>
2017-01-14 15:46:53 +0100

    Przeniesienie wszystkich testow do jednej klasy - ElementyTest

--- src/test/java/lab2_1/ElementyTest.java
+++ src/test/java/lab2_1/ElementyTest.java
@@ -49,5 +49,35 @@ public class ElementyTest {
 		assertEquals(-1, wynikWyszukiwania.getPosition());
 		assertFalse(wynikWyszukiwania.isFound());
 	}
+	
+	@Test
+	public void PojElemJestWSekwencjiTest() {
+		int key = 2;
+		int[] seq = { 2 };
+		SearchResult wynikWyszukiwania;
+		wynikWyszukiwania = BinarySearch.search(key, seq);
+		assertTrue(wynikWyszukiwania.isFound());
+		assertEquals(0, wynikWyszukiwania.getPosition());
+	}
+
+	@Test
+	public void PojElemNieJestWSekwencjiTest() {
+		int key = 3;
+		int[] seq = { 2 };
+		SearchResult wynikWyszukiwania;
+		wynikWyszukiwania = BinarySearch.search(key, seq);
+		assertFalse(wynikWyszukiwania.isFound());
+		assertEquals(-1, wynikWyszukiwania.getPosition());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void PustaSekwencjaTest() {
+		int [] seq = { };
+		if (seq.length == 0)
+		{
+			throw new IllegalArgumentException("Dlugosc sekwencji wynosi 0");
+		}
+		
+	}
 
 }
